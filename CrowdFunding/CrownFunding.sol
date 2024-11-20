55c12d50cdf2d9c0843cd2949c865100db2abcab
songhanpoo <19526981.hung@student.iuh.edu.vn>
2024-11-20 22:46:34 +0700

    :memo: append emit func for send notify when receive fund or withdraw

--- CrowdFunding/CrownFunding.sol
+++ CrowdFunding/CrownFunding.sol
@@ -9,6 +9,13 @@ contract CrownFunding {
     uint256 public MINIMUM_FUND = 5e18; // 5usd in Wei
     address public immutable i_owner;
 
+    mapping(address funder => bool isFunded) public isFunders;
+    mapping(address funder => uint256 value) public funderToAmount;
+    address[] public funders;
+
+    event Funded(address indexed funder, uint256 value);
+    event Withdraw(address indexed owner, uint256 value);
+
     receive() external payable {
         fund();
     }
@@ -33,15 +40,32 @@ contract CrownFunding {
             msg.value.getConversationRate() >= MINIMUM_FUND,
             "Your amount is insufficient, at least 0.001 ether once transaction"
         );
+
+        funderToAmount[msg.sender] += msg.value;
+        bool isFundded = isFunders[msg.sender];
+        if(!isFundded) {
+            funders.push(msg.sender);
+            isFunders[msg.sender] = true;
+        }
+        emit Funded(msg.sender, msg.value);
     }
 
     function withdraw() public ownerOnly {
-        // payable(i_owner).transfer(address(this).balance);
+        
+
+        // funders = new address[](0);
+
         (bool sent, ) = payable(i_owner).call{value: address(this).balance}("");
         require(sent, "Failed to withdraw Ethers");
+        emit Withdraw(i_owner, address(this).balance);
+        // delete  funders
     }
 
     function getBalance() public view returns (uint256) {
         return address(this).balance;
+    }
+
+    function getNumberOfFunders() public  view returns(uint256) {
+        return funders.length;
     }
 }
